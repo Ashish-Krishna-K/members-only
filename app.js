4b6bb4a09bcdcdff195d9982f6d9b5ec6d069f8d
ashish-krishna-k <ashishk.krishna@gmail.com>
2023-10-11 12:00:38 +0530

    Resets project to fresh state

--- app.js
+++ app.js
@@ -1,53 +1,41 @@
-const express = require("express");
-require('dotenv').config();
-const createError = require("http-errors");
-const path = require("path");
-const session = require("express-session");
-const passport = require("passport");
-const mongoose = require("mongoose");
-const compression = require("compression");
-const helmet = require("helmet");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const mongoDB = process.env.MONGO_URL;
-mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "mongo connection error"));
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const port = process.env.PORT || 3000;
+var app = express();
 
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/userRouter');
-const messageRouter = require('./routes/messageRouter');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-const app = express();
-
-app.use(helmet());
-app.use(compression()); // Compress all routes
-app.use(express.static(path.join(__dirname, "public")));
-app.use(session({ secret: "mugiwara", resave: false, saveUninitialized: true }));
+app.use(logger('dev'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(function (req, res, next) {
-  res.locals.currentUser = req.user;
-  next();
-});
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/user', userRouter);
-app.use('/message', messageRouter);
-app.use((req, res, next) => {
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use((err, req, res, next) => {
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
-app.listen(port)
+module.exports = app;
